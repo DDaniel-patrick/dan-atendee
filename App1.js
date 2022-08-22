9b30e1d3479f3b104a2e306a3b42ee092944f9bd
DDaniel-patrick <85047939+DDaniel-patrick@users.noreply.github.com>
2022-08-22 16:44:27 +0100

    Create App1.js

---
+++ App1.js
@@ -0,0 +1,19 @@
+app.get("/login",(req,res)=>{
+    res.render('login', {msg:""});
+})
+app.get("/home", (req, res)=>{
+res.redirect("/");
+})
+
+app.get("/login",(req,res)=>{
+    res.render('login', {msg:""});
+})
+app.get("/home", (req, res)=>{
+res.redirect("/");
+})
+app.get("/login",(req,res)=>{
+    res.render('login', {msg:""});
+})
+app.get("/home", (req, res)=>{
+res.redirect("/");
+})
